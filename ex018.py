5df719951c4a348676c38c25d00869a7be297666
Ronald Pereira Evangelista <Ronald.Evangelista@Aluno.ifsp.edu.br>
2022-11-13 17:01:58 -0300

    Exercise 18th done.

---
+++ ex018.py
@@ -0,0 +1,12 @@
+from math import sin, cos, tan
+n1 = float(input("Insira um ângulo: "))
+
+# Using imports from math:
+print("O valor do seno é: {:.2f}".format(sin(n1)))
+print("O valor do cosseno é de: {:.2f}".format(cos(n1)))
+print("O valor da tangente é de: {:.2f}".format(tan(n1)))
+
+# Using not so basic math:
+
+# I'm not good enough to search just in 1 minute and find a simplified calc. So, for don't decelerate my studys, I pass
+# this challenge.
