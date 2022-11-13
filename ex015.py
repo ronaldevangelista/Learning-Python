8c060b231ad71054a919107873f67b1e78f2bc53
Ronald Pereira Evangelista <Ronald.Evangelista@Aluno.ifsp.edu.br>
2022-11-13 14:45:43 -0300

    The 14th and 15th exercises done. I'll improve the 15th.

---
+++ ex015.py
@@ -0,0 +1,3 @@
+dias = int(input("O carro foi alugado por quantos dias? "))
+km = float(input("Quantos quilometros foram rodados? "))
+print("O valor final a ser pago é de: {} reais".format(dias * 60 + km * 0.15))
