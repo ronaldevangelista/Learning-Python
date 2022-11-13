f0a8b4ba9e0394aaff5783f2b3f0a8c191f00641
Ronald Pereira Evangelista <Ronald.Evangelista@Aluno.ifsp.edu.br>
2022-11-13 15:01:20 -0300

    15th exercise was improved. I won't use if statements until the course explain. I must not surpasse the activity too much.

--- ex015.py
+++ ex015.py
@@ -1,3 +1,7 @@
 dias = int(input("O carro foi alugado por quantos dias? "))
 km = float(input("Quantos quilometros foram rodados? "))
-print("O valor final a ser pago é de: {} reais".format(dias * 60 + km * 0.15))
+valor = float(dias * 60 + km * 0.15)
+print("\nO valor a ser pago é de: {} reais".format(valor))
+# improving the code.
+parc = int(input("\nEm quantas parcelas você deseja pagar? "))
+print("São ao todo {} parcela(s) no valor de {} reais.".format(parc, valor / parc))
