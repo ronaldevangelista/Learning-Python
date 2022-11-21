a368cc378ab2cc94f4fa1b413bbfa14addf6821b
ronald.evangelista@aluno.ifsp.edu.br <ronald.evangelista@aluno.ifsp.edu.br>
2022-11-21 09:57:27 -0300

    Finally I did the last piece of 23rd exercise. I didn't test yet.

--- ex023.py
+++ ex023.py
@@ -7,3 +7,7 @@ print("Centena(s): {:.0f}".format((n1 % 1000 - (n1 % 100)) / 100))
 print("Milhar(es): {:.0f}".format((n1 % 10000 - (n1 %1000)) / 1000))
 
 #String Manipulation Way:
+print("Unidade(s): {}".format(n1[3]))
+print("Dezena(s): {}".format(n1[2]))
+print("Centena(s): {}".format(n1[1]))
+print("Milhares(es): {0}".format(n1[0]))
