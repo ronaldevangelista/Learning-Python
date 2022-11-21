d49c6519a487648a64549eb7d7a98fe4cc6118a2
Ronald Pereira <Ronald.Evangelista@Aluno.ifsp.edu.br>
2022-11-21 00:34:37 -0300

    I guess I solved the git problem. I've always to pull the code before I start to work at the principal branch.

--- ex022.py
+++ ex022.py
@@ -1,1 +1,8 @@
-# I'm testing a mobile IDE. If everything gets ok, probaly it'll increase my learning speed.
+nome = input("Insira o nome de uma pessoa: ")
+
+print("O nome com todas as letras maiúsculas: {}".format(nome.upper()))
+print("O nome com todas as letras minúsculas: {}".format(nome.lower()))
+
+print("Quantas letras ao todo (Sem espaços): {}".format(len(nome) - nome.count(" ")))
+
+print("Quantas letras tem o primeiro nome: {}".format(len(nome.split()[0])))
